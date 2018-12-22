86463b09971be9d51bee47e0896b9d2a6255aa09
Xhuliano Brace <34151358+xhulianoThe1@users.noreply.github.com>
2018-12-21 19:09:14 -0500

    Update caesar.c
    Cleaning the code

--- caesar.c
+++ caesar.c
@@ -1,47 +1,44 @@
-
 #include <stdio.h>
 #include <ctype.h>
 #include <stdlib.h>
 #include <string.h>
 
 int main(int argc,char* argv[]) { 
-	//initialize key to 0 and the manipulate based on CLA 
+   //key's value will be based on command line argument 
    int key = 0; 
-   //If CLA isnt equal to 3 then you cant do this program
-   if(argc!=3)  { 
+   //Only allow 3 command line arguments 
+   if(argc!=3) { 
         printf("\nInvalid amount of command line args "); 
-    } 
-		
-	else { 
-			
-			//the first argument inputted is equal to the key var 
-			key = atoi(argv[1]); 
-			int i = 0;
-			//Message for inputted text
-			char msg[100];
-			printf("Enter a Mesage: ");
-			scanf ("%[^\n]%*c", msg);
-			printf("Your inputted text is: %s\n", msg);
+    } 	
+       
+	//performs if the correct amount of comm line args are inputted 
+	else { 		
+		key = atoi(argv[1]); 
+		int i = 0;
+		//Message for inputted text
+		char msg[100];
+		printf("Enter a Mesage: ");
+		scanf ("%[^\n]%*c", msg);
 		
 		//If the second argument is equal to e then do this 	 
 		if (*argv[2] == 'e'){ 
-		//FOr loop that performs caesar ciph for encryption 
+		
+			//For loop that performs caesar ciph for encryption 
 			for(i = 0; i < strlen(msg); i++){ 
 				if (isalpha(msg[i])){		
-					printf("%c\n", ((((msg[i] - 65) + key) % 26) + 65));
+					printf("%c\n", ((((msg[i] - 65) + key) % 26) + 65)); 
 				} 
 			} 	
 		} 
 			
-		//If second arg isnt equal to e and in this case d then do this
+		//loop that performs caesar cipher for decrypting
 		else { 
-		//FOr loop for decrypting
 			for(i = 0; i < strlen(msg); i++){ 
 				if (isalpha(msg[i])){			
 					printf("%c\n", ((((msg[i] - 65) - key) % 26) + 65));
 			} 
 		}		 			
-	 } 
 	} 
+} 
 	   return 0; 
- } 
+} 
